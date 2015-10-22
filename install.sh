125e5f47bd735efd7c778db02e00b606d38b0ec8
Jason Poll <jason.poll@ccisystems.com>
2015-10-21 19:20:32 -0500

    updated install

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #bin/bash 
+gem install snipcheat
 cp *.sublime-snippet ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 echo Should be good to go
+snipcheat generate -d . -o ./snippets_cheatsheet.html
+open ./snippets_cheatsheet.html
